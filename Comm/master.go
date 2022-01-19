f06e7e9c5434c8dabf6026ca9c4fb9ae393f6846
sevpinna <nisedt@163.com>
2022-01-19 10:23:14 +0800

    主站报文构建函数完成

--- Comm/master.go
+++ Comm/master.go
@@ -1,53 +1,71 @@
 package Comm
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 // BuildReadCoilStatus  构建读取线圈状态报文
 func BuildReadCoilStatus(Id uint8, RegisterAddress, Length uint16) (Message []byte) {
-	Message = BytesCombine([]byte{Id}, []byte[]{0x01}, Uint16ToByte(RegisterAddress), Uint16ToByte(Length))
+	Message = BytesCombine([]byte{Id}, []byte{0x01}, Uint16ToByte(RegisterAddress), Uint16ToByte(Length))
 	return
 }
 
 // BuildReadInputStatus 构建读取输入状态报文
 func BuildReadInputStatus(Id uint8, RegisterAddress, Length uint16) (Message []byte) {
-	Message = BytesCombine([]byte{Id}, []byte[]{0x02}, Uint16ToByte(RegisterAddress), Uint16ToByte(Length))
+	Message = BytesCombine([]byte{Id}, []byte{0x02}, Uint16ToByte(RegisterAddress), Uint16ToByte(Length))
 	return
 }
 
 // BuildReadHoldingRegister 构建读取保持寄存器报文
 func BuildReadHoldingRegister(Id uint8, RegisterAddress, Length uint16) (Message []byte) {
-	Message = BytesCombine([]byte{Id}, []byte[]{0x03}, Uint16ToByte(RegisterAddress), Uint16ToByte(Length))
-	return nil
+	Message = BytesCombine([]byte{Id}, []byte{0x03}, Uint16ToByte(RegisterAddress), Uint16ToByte(Length))
+	return
 }
 
 // BuildReadInputRegister 构建读取输入寄存器报文
 func BuildReadInputRegister(Id uint8, RegisterAddress, Length uint16) (Message []byte) {
-	Message = BytesCombine([]byte{Id}, []byte[]{0x04}, Uint16ToByte(RegisterAddress), Uint16ToByte(Length))
-	return nil
+	Message = BytesCombine([]byte{Id}, []byte{0x04}, Uint16ToByte(RegisterAddress), Uint16ToByte(Length))
+	return
 }
 
 // BuildWriteSingleCoilStatus 构建写单个线圈报文
 func BuildWriteSingleCoilStatus(Id uint8, RegisterAddress uint16, Data bool) (Message []byte) {
-	Message = BytesCombine([]byte{Id}, []byte[]{0x05}, Uint16ToByte(RegisterAddress))
-	return nil
+	var data = make([]byte, 1)
+	if Data {
+		data[0] = 1
+	} else {
+		data[0] = 0
+	}
+	Message = BytesCombine([]byte{Id}, []byte{0x05}, Uint16ToByte(RegisterAddress), data)
+	return
 }
 
 // BuildWriteSingleHoldingRegister 构建写单个保持寄存器报文
-func BuildWriteSingleHoldingRegister(Id uint8, RegisterAddress, Length uint16, Data Register) (Message []byte) {
-	Message = BytesCombine([]byte{Id}, []byte[]{0x10}, Uint16ToByte(RegisterAddress), []byte{1, 2})
-	return nil
+func BuildWriteSingleHoldingRegister(Id uint8, RegisterAddress uint16, Data []byte) (Message []byte) {
+	Message = BytesCombine([]byte{Id}, []byte{0x06}, Uint16ToByte(RegisterAddress), []byte{1, 2}, Data)
+	return
 }
 
 // BuildWriteMultipleCoilStatus 构建写多个线圈报文
 func BuildWriteMultipleCoilStatus(Id uint8, RegisterAddress uint16, Data []bool) (Message []byte) {
-	lenf := math.Ceil(float64(len(Data) / 8))
-	Message = BytesCombine([]byte{Id}, []byte[]{0x0f}, Uint16ToByte(RegisterAddress), Uint16ToByte(uint16(len(Data))), Uint16ToByte(uint16(lenf)))
-	return nil
+	lenF := uint16(math.Ceil(float64(len(Data)) / 8))
+	fmt.Println(lenF)
+	var data = make([]byte, lenF)
+	fmt.Println(data)
+	for i := 0; i < int(lenF); i++ {
+		for j := 0; j < len(Data)-i*8; j++ {
+			if Data[i*8+j] {
+				data[i] = data[i] + uint8(1<<j)
+			}
+		}
+	}
+	Message = BytesCombine([]byte{Id}, []byte{0x0f}, Uint16ToByte(RegisterAddress), Uint16ToByte(uint16(len(Data))), []byte{uint8(lenF)}, data)
+	return
 }
 
 // BuildWriteMultipleHoldingRegister 构建写多个保持寄存器报文
-func BuildWriteMultipleHoldingRegister(Id uint8, RegisterAddress uint16, Data Registers) (Message []byte) {
-
-	Message = BytesCombine([]byte{Id}, []byte[]{0x10}, Uint16ToByte(RegisterAddress), Uint16ToByte(Data.Len()), Uint16ToByte(Data.Len()*2))
-	return nil
+func BuildWriteMultipleHoldingRegister(Id uint8, RegisterAddress uint16, Data []byte) (Message []byte) {
+	Message = BytesCombine([]byte{Id}, []byte{0x10}, Uint16ToByte(RegisterAddress), Uint16ToByte(uint16(len(Data)/2)), []byte{uint8(len(Data))}, Data)
+	return
 }
